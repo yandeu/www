339ca317daa344c9cc4c0cae327036e16a404b06
Yannick <20306025+yandeu@users.noreply.github.com>
2023-05-31 18:49:49 +0200

    Update index.js

--- project2/index.js
+++ project2/index.js
@@ -5,3 +5,24 @@ expect(age).toBe(24)
 // TODO: Füge den Nachnamen "Müller" hinzu.
 let name = "John"
 expect(name).toBe("John Müller")
+
+// TODO: Bringe den Hund zum Bellen.
+class Animal {
+  type
+  
+  constructor(type) {
+    this.type = type
+  }
+
+  makeSound() {
+    if(this.type === "dog") {
+      return "Wuff"
+    }
+    else if(this.type === "cat") {
+      return "Miau"
+    }
+  }
+}
+let animal = new Animal()
+let sound = animal.makeSound()
+expect(sound).toBe("Wuff")
